174a8f5c0831c4cbf960bc28afcfd4df4dfc2d9d
Hadole Raju <67407713+rajureddyhadole@users.noreply.github.com>
2024-10-06 21:21:00 +0530

    useMutation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 
 const App = () => {
   const { data, error, isLoading } = useQuery({
@@ -8,12 +8,30 @@ const App = () => {
         .then((res) => res.json())
   });
 
-  if (error) return <div>there was an Error</div>;
+  const { mutate, isPending, isError, isSuccess } = useMutation({
+    mutationFn: (newPost) =>
+      fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: JSON.stringify(newPost),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+      }).then((res) => res.json())
+  })
 
-  if (isLoading) return <h1>Loading...</h1>
+  if (error || isError) return <div>there was an Error</div>;
+
+  if (isLoading) return <h1>Loading...</h1>;
 
   return (
     <div>
+      {isPending && <p>DATA IS BEING ADDED...</p>}
+      <button onClick={() => mutate({
+        userId: 1000,
+        id: 1111,
+        title: "this is a header written by raju reddy.",
+        body: "this is a new post."
+      })}>
+        add post
+      </button>
       {data.map((post) => (
         <div key={post.id}>
           <h3>title: {post.title}</h3>
